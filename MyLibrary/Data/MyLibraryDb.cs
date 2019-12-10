73437e2cd1cd540cc115e74525e3f6f3734c47de
User <user.email@mail.org>
2019-12-10 14:13:15 +0200

    Project upload

---
+++ MyLibrary/Data/MyLibraryDb.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class MyLibraryDb : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\ProjectsV13;Initial Catalog=MyLibraryDb;");
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+    }
+}
